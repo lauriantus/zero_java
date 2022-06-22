5185cd03e1efb7adb43a0c112f9cf957f9e4ca20
lauriantus <lauriantus@gmail.com>
2022-06-22 17:49:22 +0300

    zero on java

--- src/com/practice/task8_square.java
+++ src/com/practice/task8_square.java
@@ -1,4 +1,17 @@
 package com.practice;
 
+import java.util.Scanner;
+
 public class task8_square {
+    public static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Enter the size of square, please:");
+        int size = scanner.nextInt();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println(" ");
+        }
+    }
 }
