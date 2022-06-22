25b2f7a5356286e65a6a405fb59999bd944c7f59
lauriantus <lauriantus@gmail.com>
2022-06-22 18:01:32 +0300

    zero on java

--- src/com/practice/task8_square.java
+++ src/com/practice/task8_square.java
@@ -4,14 +4,37 @@ import java.util.Scanner;
 
 public class task8_square {
     public static Scanner scanner = new Scanner(System.in);
+    public static int size;
     public static void main(String[] args) {
         System.out.println("Enter the size of square, please:");
-        int size = scanner.nextInt();
+        size = scanner.nextInt();
+        System.out.println("With for:");
+        funcFOR();
+        System.out.println("With while:");
+        funcWHILE();
+    }
+
+    private static void funcFOR() {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 System.out.print(" * ");
             }
             System.out.println(" ");
         }
+    }
+
+    private static void funcWHILE() {
+        int i = 0;
+        int j = 0;
+        while (i < size) {
+            while (j < size) {
+                System.out.print(" * ");
+                j++;
+            }
+            j = 0;
+            System.out.println(" ");
+            i++;
+        }
+
     }
 }
