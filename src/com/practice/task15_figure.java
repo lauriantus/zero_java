281df7c00b25a236e9e682fc2c09d04053236db3
lauriantus <lauriantus@gmail.com>
2022-06-25 11:17:52 +0300

    in process

--- src/com/practice/task15_figure.java
+++ src/com/practice/task15_figure.java
@@ -9,24 +9,31 @@ public class task15_figure {
     public static int [] high = new int [20];
 
     public static void main(String[] args) {
-        for (int i = 0; i < 20; i++) {
+        int i = 0;
+        while (i < 20) {
             System.out.println("Enter the number of stars in the row " + i + ":");
             high[i] = in.nextInt();
 
-            if (high[i] < 0) {  //cancel iteration
-                i--;
+            if (high[i] <= 0 || high[i] % 2 == 0) {  //cancel iteration
+
+                //if cancel
+                if (high[i] == 0) {
+                    size = i;
+                    break;
+                }
+
+                //if wrong enter(pass)
+                continue;
             }
 
-            if (high[i] == 0) { //off
-                size = i;
-                break;
-            }
-
-            if (max < high[i]) { //max long
+            //max long
+            if (max < high[i]) {
                 max = high[i];
             }
+            i++;
         }
-        for (int i = 0; i < size; i++) {
+        // print figure
+        for (i = 0; i < size; i++) {
             int space = (max - high[i])/2;
             for (int j = 0; j < space; j++) {
                 System.out.print(" ");
